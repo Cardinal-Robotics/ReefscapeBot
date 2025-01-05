8af34e5b60fe169cff7f1eac7f41895a6d908937
Cardinal156 <2027156@scprep.org>
2025-01-05 14:27:29 -0500

    Throw in some constants from last year.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,3 +1,4 @@
+// 1/27/24 Changed kDriveEncoderPerPulse added "* .093433" to correctly relate encoder rotations to distance in meters
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
@@ -16,8 +17,48 @@ package frc.robot;
  * wherever the
  * constants are needed, to reduce verbosity.
  */
+
 public final class Constants {
-    public static class OperatorConstants {
+    public static final class OperatorConstants {
         public static final int kDriverControllerPort = 0;
+        public static final int kOperatorControllerPort = 1;
     }
+
+    public static final class DriveConstants {
+        public static final int kFrontLeftDriveMotorPort = 2;
+        public static final int kRearLeftDriveMotorPort = 5;
+        public static final int kFrontRightDriveMotorPort = 11;
+        public static final int kRearRightDriveMotorPort = 8;
+
+        public static final int kFrontLeftTurningMotorPort = 1;
+        public static final int kRearLeftTurningMotorPort = 4;
+        public static final int kFrontRightTurningMotorPort = 10;
+        public static final int kRearRightTurningMotorPort = 7;
+
+        public static final int kFrontLeftCancoderPort = 3;
+        public static final int kRearLeftCancoderPort = 6;
+        public static final int kFrontRightCancoderPort = 12;
+        public static final int kRearRightCancoderPort = 9;
+
+        public static final boolean kFrontLeftTurningEncoderReversed = false;
+        public static final boolean kRearLeftTurningEncoderReversed = true;
+        public static final boolean kFrontRightTurningEncoderReversed = false;
+        public static final boolean kRearRightTurningEncoderReversed = true;
+
+        public static final boolean kFrontLeftDriveEncoderReversed = false;
+        public static final boolean kRearLeftDriveEncoderReversed = true;
+        public static final boolean kFrontRightDriveEncoderReversed = false;
+        public static final boolean kRearRightDriveEncoderReversed = true;
+
+        public static final boolean kFrontLeftNotVersH = true;
+        public static final boolean kRearLeftNotVersH = false;
+        public static final boolean kFrontRightNotVersH = true;
+        public static final boolean kRearRightNotVersH = false;
+
+        public static final String kFrontLeftName = "FL";
+        public static final String kRearLeftName = "RL";
+        public static final String kFrontRightName = "FR";
+        public static final String kRearRightName = "RR";
+    }
+
 }
