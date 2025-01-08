bd8b3c0572950b33a782d176c0e8bbcb558c1815
Cardinal156 <2027156@scprep.org>
2025-01-07 21:36:16 -0500

    Add some simulation specific code from YAGSL example

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,6 +7,7 @@ package frc.robot;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.subsystems.SwerveSubsystem;
 import swervelib.SwerveInputStream;
+import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 
@@ -50,6 +51,21 @@ public class RobotContainer {
                     m_driverController::getRightY)
             .headingWhile(true);
 
+    // YAGSL requires special logic for robot simulation. Taken from:
+    // https://github.com/BroncBotz3481/YAGSL-Example/blob/de932a45b41409442ffcc99832597906ebd34eb4/src/main/java/frc/robot/RobotContainer.java#L92-L110
+    private final SwerveInputStream m_driveAngularVelocitySim = SwerveInputStream.of(m_swerveDrive.getSwerveDrive(),
+            () -> -m_driverController.getLeftY(),
+            () -> -m_driverController.getLeftX())
+            .withControllerRotationAxis(() -> m_driverController.getRawAxis(2))
+            .deadband(OperatorConstants.kDeadband)
+            .scaleTranslation(0.8)
+            .allianceRelativeControl(true);
+
+    private final SwerveInputStream m_driveDirectAngleSim = m_driveAngularVelocitySim.copy()
+            .withControllerHeadingAxis(() -> Math.sin(m_driverController.getRawAxis(2) * Math.PI) * (Math.PI * 2),
+                    () -> Math.cos(m_driverController.getRawAxis(2) * Math.PI) * (Math.PI * 2))
+            .headingWhile(true);
+
     // ---------------------------------------------------------------------------------------------------------------------------------------
     //
 
@@ -59,12 +75,23 @@ public class RobotContainer {
     private final Command m_driveFieldOrientedDirectAngle = m_swerveDrive.driveFieldOriented(m_driveDirectAngle);
     private final Command m_driveFieldOrientedAngularVelocity = m_swerveDrive
             .driveFieldOriented(m_driveAngularVelocity);
+
+    // YAGSL requires special logic for robot simulation.
+    private final Command m_driveFieldOrientedDirectAngleSim = m_swerveDrive.driveFieldOriented(m_driveDirectAngleSim);
     // ---------------------------------------------------------------------------------------------------------------------------------------
     //
 
     public RobotContainer() {
         configureBindings();
-        m_swerveDrive.setDefaultCommand(m_driveFieldOrientedAngularVelocity);
+
+        // This is called a "Ternary operator"; When you do something along the lines of
+        // variable = (condition) ? expressionTrue : expressionFalse; variable =
+        // expressionTrue when the condition is true, and expressionFalse when the
+        // condition is false. For example:
+        // displayMessage = (2 + 2) == 22 ? "Math ain't adding up" : "Correct!";
+        // Then displayMessage will be "Correct!" because 4 doesn't equal 22.
+        m_swerveDrive.setDefaultCommand(
+                RobotBase.isSimulation() ? m_driveFieldOrientedDirectAngleSim : m_driveFieldOrientedDirectAngle);
     }
 
     private void configureBindings() {
