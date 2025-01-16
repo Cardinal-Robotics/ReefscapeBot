69c09c202d95b20b09244b50c5c88a5ce091c69c
Cardinal156 <145771162+Cardinal156@users.noreply.github.com>
2025-01-16 08:19:26 -0500

    Begin SysId support

--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -6,14 +6,16 @@ package frc.robot.subsystems;
 
 import java.util.function.Supplier;
 
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Config;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.wpilibj2.command.Command;
 
 import frc.robot.Constants.DriveConstants;
 
 import swervelib.parser.SwerveParser;
+import swervelib.SwerveDriveTest;
 import swervelib.SwerveDrive;
 
 public class SwerveSubsystem extends SubsystemBase {
@@ -40,6 +42,34 @@ public class SwerveSubsystem extends SubsystemBase {
             this.m_swerveDrive.driveFieldOriented(velocity.get());
         });
     }
+
+      /**
+   * Command to characterize the robot drive motors using SysId
+   *
+   * @return SysId Drive Command
+   */
+  public Command sysIdDriveMotorCommand()
+  {
+    return SwerveDriveTest.generateSysIdCommand(
+        SwerveDriveTest.setDriveSysIdRoutine(
+            new Config(),
+            this, m_swerveDrive, 12, true),
+        3.0, 5.0, 3.0);
+  }
+
+  /**
+   * Command to characterize the robot angle motors using SysId
+   *
+   * @return SysId Angle Command
+   */
+  public Command sysIdAngleMotorCommand()
+  {
+    return SwerveDriveTest.generateSysIdCommand(
+        SwerveDriveTest.setAngleSysIdRoutine(
+            new Config(),
+            this, m_swerveDrive),
+        3.0, 5.0, 3.0);
+  }
 
     public SwerveDrive getSwerveDrive() {
         return this.m_swerveDrive;
